6c9822f1ef222bc09ec5f4485ec5063bb3edaab3
Colman Currie <curriecolman@gmail.com>
2020-03-02 18:20:03 -0800

    create bones of project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header.jsx';
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+    < Header />
+
+      <h1>FeedR Pdx</h1>
+    </div>
+  );
+}
+
+export default App;
