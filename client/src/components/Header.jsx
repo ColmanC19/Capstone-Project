776b3c90deecbd791f93ca9adc5d3af525483819
Colman Currie <curriecolman@gmail.com>
2020-03-04 13:48:33 -0800

    add styling

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 
 
 function Header(){
-
+const headerCSS = {
+  fontSize: "70px",
+  textAlign: "center"
+}
 
 
  return (
 
    <div>
-   <h1>FeedR Pdx</h1>
-    <Link to="/homebody"> Home </Link><Link to="/"> Back </Link><Link to="/restaurantguide"> Restaurant Guide </Link>
+    <Link to="/homebody"> Home| </Link><Link to="/"> Back| </Link><Link to="/restaurantguide"> Restaurant Guide| </Link>
+    <h1 style={headerCSS}>FeedR Pdx</h1>
 
 
    </div>
