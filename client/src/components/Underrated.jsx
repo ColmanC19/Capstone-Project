1186e4d2ccbd987a2d35f28e4702c9e0b814541c
Colman Currie <curriecolman@gmail.com>
2020-03-06 16:33:28 -0800

    add api for articles

--- client/src/components/Underrated.jsx
+++ client/src/components/Underrated.jsx
@@ -1,16 +1,54 @@
 import React from 'react';
 
-function Underrated(){
-const underratedCSS = {
-  backgroundColor: "black",
+class Underrated extends React.Component {
 
-}
-  return(
-    <div>
-    <h1>These Carts are Criminally insane!</h1>
+  constructor(props){
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      food: []
+    };
+  }
 
-    </div>
-  )
-}
+  componentDidMount() {
+      fetch('https://api.currentsapi.services/v1/latest-news?apiKey=ILOrXZ77NUMCwgnFltIXJbJ9GJ9XkS0HjzF-sreBrWOFD6kT&language=en')
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            food: result.news[0].title
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+    render(){
+
+
+      const { error, isLoaded, items } = this.state;
+     if (error) {
+       return <div>Error: {error.message}</div>;
+     } else if (!isLoaded) {
+       return <div>Loading...</div>;
+     } else {
+       return (
+         <ul>
+          <h1> {this.state.food} </h1>
+         </ul>
+       );
+     }
+   }
+ }
 
 export default Underrated
