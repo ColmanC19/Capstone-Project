9662c4ccd7259f2c7b56a331ef0762924b62b501
Colman Currie <curriecolman@gmail.com>
2020-03-10 15:11:55 -0700

    deploy

--- client/src/components/RestaurantGuide.jsx
+++ client/src/components/RestaurantGuide.jsx
@@ -4,8 +4,6 @@ import "bootstrap/dist/css/bootstrap.css";
 import { Redirect } from 'react-router-dom';
 import { Grid } from 'react-bootstrap';
 import './Underrated.css';
-import CommentList from "./CommentList";
-import CommentForm from "./CommentForm";
 
 
 
@@ -21,41 +19,10 @@ class RestaurantGuide extends React.Component{
       redirectfeedr: false,
       error: null,
       isLoaded: false,
-      comments: [],
-      loading: false
+
     };
-    this.addComment = this.addComment.bind(this);
   };
 
-
-  componentDidMount() {
-    // loading
-    this.setState({ loading: true });
-
-    // get all the comments
-    fetch("http://localhost:7777")
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          comments: res,
-          loading: false
-        });
-      })
-      .catch(err => {
-        this.setState({ loading: false });
-      });
-  }
-
-  /**
-   * Add new comment
-   * @param {Object} comment
-   */
-  addComment(comment) {
-    this.setState({
-      loading: false,
-      comments: [comment, ...this.state.comments]
-    });
-  }
 
   setRedirect = () => {
     this.setState({
@@ -131,7 +98,6 @@ class RestaurantGuide extends React.Component{
 
 
   render() {
-    const loadingSpin = this.state.loading ? "App-logo Spin" : "App-logo";
     const btnCSS = {
       display: "inline-flex",
       // padding: "50px",
@@ -191,36 +157,6 @@ class RestaurantGuide extends React.Component{
 
 
     </Grid>
-    <div className="App container bg-light shadow">
-      <header className="App-header">
-
-        <h1 className="App-title">
-          React Comments
-          <span className="px-2" role="img" aria-label="Chat">
-            💬
-          </span>
-        </h1>
-        <p>
-          Checkout the tutorial on{" "}
-          <a className="text-light" href="https://qcode.in">
-            QCode.in
-          </a>
-        </p>
-      </header>
-
-      <div className="row">
-        <div className="col-4  pt-3 border-right">
-          <h6>Say something about React</h6>
-          <CommentForm addComment={this.addComment} />
-        </div>
-        <div className="col-8  pt-3 bg-white">
-          <CommentList
-            loading={this.state.loading}
-            comments={this.state.comments}
-          />
-        </div>
-      </div>
-    </div>
 
     </div>
     )
